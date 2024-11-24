589eb29c487e0c1ddf0843eef5d6be7aa4b10795
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2024-11-24 10:12:55 -0500

    feat: :sparkles: playing with variables

---
+++ session_1/variables.py
@@ -0,0 +1,27 @@
+import sys
+
+
+# x = "Hola mundo"
+# print(x)
+# print(type(x))
+# print(id(x))
+# print("Memory (bytes): ", sys.getsizeof(x))
+
+# x = 20
+# print(x)
+# print(type(x))
+# print(id(x))
+# print("Memory (bytes): ", sys.getsizeof(x))
+
+# x = 13.3
+# print(x)
+# print(type(x))
+# print(id(x))
+# print("Memory (bytes): ", sys.getsizeof(x))
+
+y = True
+print(type(y))
+y = False
+print(type(y))
+print("Memory (bytes): ", sys.getsizeof(y))
+
